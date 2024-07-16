c432c1cfbe4742389832c427a78ea2079338ea0a
kolasaniv1996 <92003218+kolasaniv1996@users.noreply.github.com>
2024-07-16 18:29:06 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -11,4 +11,4 @@ job_n=$WORLD_SIZE
 #echo ${master_addr}
 #echo ${master_port}
 
-LOGLEVEL="INFO" torchrun --nproc_per_node=1 --nnodes=2:${job_n} --max-restarts=3 --rdzv_endpoint=${master_addr}:${master_port} --rdzv_backend=c10d elastic-distributed.py --batch_size 8 1000 2
+LOGLEVEL="INFO" torchrun --nproc_per_node=1 --nnodes=2:${job_n} --max-restarts=3 --rdzv_endpoint=${master_addr}:${master_port} --rdzv_backend=c10d distributed.py --batch_size 8 1000 2
